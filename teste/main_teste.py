03066360be99b69a5c68ac1f04ab110cbd7ae282
wellington-costa <wellington_costa@live.com>
2023-01-26 17:25:33 -0300

    "Adicionando tela de lista de produtos"

--- teste/main_teste.py
+++ teste/main_teste.py
@@ -14,24 +14,6 @@ from kaki.app import App
 class ShowProductsScreen(Screen):
     pass
 
-class CadProdScreen(Screen):
-
-    def clear(self):
-        self.manager.get_screen('CadProdScreen').ids.desc_product.text=""
-        self.manager.get_screen('CadProdScreen').ids.marca.text = ""
-        self.manager.get_screen('CadProdScreen').ids.cod_product.text = ""
-        self.manager.get_screen('CadProdScreen').ids.prec_compra.text = ""
-        self.manager.get_screen('CadProdScreen').ids.prec_atacado.text = ""
-        self.manager.get_screen('CadProdScreen').ids.prec_varejo.text = ""
-        self.manager.get_screen('CadProdScreen').ids.qtd.text = ""
-        self.manager.get_screen('CadProdScreen').ids.cb.text = ""
-
-    def register(self, descricao, marca, cod_product, qtd, prec_compra, prec_atacado, prec_varejo, cb ):
-       if registerProduct(descricao, marca, cod_product, qtd, prec_compra, prec_atacado, prec_varejo, cb):
-        self.clear()
-
-
-
 
 
 class TesteApp(MDApp, App):
--- teste/screenmanager.py
+++ teste/screenmanager.py
@@ -1,6 +1,31 @@
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.scrollview import ScrollView
+
+from core import *
+from models import *
+
 
 screen = ScreenManager()
 
 class MainScreenManager(ScreenManager):
+    pass
+
+
+class CadProdScreen(Screen):
+
+    def clear(self):
+        self.manager.get_screen('CadProdScreen').ids.desc_product.text=""
+        self.manager.get_screen('CadProdScreen').ids.marca.text = ""
+        self.manager.get_screen('CadProdScreen').ids.cod_product.text = ""
+        self.manager.get_screen('CadProdScreen').ids.prec_compra.text = ""
+        self.manager.get_screen('CadProdScreen').ids.prec_atacado.text = ""
+        self.manager.get_screen('CadProdScreen').ids.prec_varejo.text = ""
+        self.manager.get_screen('CadProdScreen').ids.qtd.text = ""
+        self.manager.get_screen('CadProdScreen').ids.cb.text = ""
+
+    def register(self, descricao, marca, cod_product, qtd, prec_compra, prec_atacado, prec_varejo, cb ):
+       if registerProduct(descricao, marca, cod_product, qtd, prec_compra, prec_atacado, prec_varejo, cb):
+        self.clear()
+
+class ListProdScreen(Screen):
     pass
